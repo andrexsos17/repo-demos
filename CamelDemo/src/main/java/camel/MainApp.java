cc662907b964a1410e20234b9c2620bcf711b6d8
andrexsos17 <andrexsos17@hotmail.com>
2019-11-30 22:42:00 -0500

    se cambia el main para utilizar el main de camel

--- CamelDemo/src/main/java/camel/MainApp.java
+++ CamelDemo/src/main/java/camel/MainApp.java
@@ -3,26 +3,29 @@ package camel;
 import org.apache.camel.CamelContext;
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.impl.DefaultCamelContext;
+import org.apache.camel.main.Main;
 
 import java.io.File;
 
 public class MainApp {
-    public static void main (String [] args) throws Exception {
-        //String homeDir = System.getProperty("home.dir");
-        File file = new File("/home/andressossa/test1.txt");
+    private Main main;
 
-        System.out.println(file.canRead()+"_"+file.exists());
+    public static void main(String[] args) throws Exception {
+        MainApp example = new MainApp();
+        example.boot();
+    }
 
+    public void boot() throws Exception {
+        // create a Main instance
+        main = new Main();
+        // enable hangup support so you can press ctrl + c to terminate the JVM
+        main.enableHangupSupport();
+        // add routes
+        main.addRoutesBuilder(new MoveFile());
 
-        CamelContext ctx = new DefaultCamelContext();
-        RouteBuilder route = new MoveFile();
-        ctx.addRoutes(route);
-        ctx.start();
-        // Maybe sleep a little here
-        Thread.sleep(2000);
-        ctx.stop();
-
-        System.out.println("hola mundo");
+        // run until you terminate the JVM
+        System.out.println("Starting Camel. Use ctrl + c to terminate the JVM.\n");
+        main.run();
     }
 
 }
